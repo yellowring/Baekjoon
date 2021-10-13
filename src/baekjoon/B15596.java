9e9ba8683114203c251b151a21d243e5f7b57f74
greenyellowring <92421819+greenyellowring@users.noreply.github.com>
2021-10-13 17:48:52 +0900

    message

--- src/baekjoon/B15596.java
+++ src/baekjoon/B15596.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class B15596 {
     long sum(int[] a) {
-			long sum = 0;	// a 배열 정수 합 변수
+			long sum = 0;	// 
 	        
 			for(int i = 0; i < a.length; i++) {
 				sum += a[i];
 			}
-			return sum; 
+			return sum;    
 		}
 }
