32f2f7d42ea217e5c8114e826fb8a56e58064379
greenyellowring <92421819+greenyellowring@users.noreply.github.com>
2021-10-18 15:18:56 +0900

    commit aaaa

--- src/baekjoon/B1065.java
+++ src/baekjoon/B1065.java
@@ -1,3 +1,5 @@
+package baekjoon;
+
 import java.io.*;
 import java.util.*;
 import java.util.stream.Stream;
@@ -7,36 +9,25 @@ public class B1065 {
 
 		Scanner in = new Scanner(System.in);
 
-		int N = in.nextInt(); // N 입력 받음;
-		in.close();
+		int N = in.nextInt(); 
+		int result = 0;
 		
-		int count = 0;
-
-		if(N<100) 
-		{ 
-			System.out.println(N);
-		}
-		
-		else 
-		{
-			count = 99;
-			if(N == 1000) {
-				N = 999;
+		for(int i = 0 ; i < N ; i ++) {
+			
+			int number = i;
+			int sum = 0;
+			
+			while(number != 0) {
+				sum += number % 10;
+				number /= 10;
 			}
-			for (int i = 100; i <= N; i++) {
-				
-				int bak = i/100;
-				int sib = (i/10)%10;
-				int ill = i%10;
-				
-				if(bak-sib == sib-ill) {
-					count++;
-				}
-
+			
+			if(i+sum==N) {
+				result = i;
+				break;
 			}
-			System.out.println(count);
-
 		}
+		System.out.println(result);
 		
 	}
 }
